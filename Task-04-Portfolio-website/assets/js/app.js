23d6ebfbf73bb0a11fa1e2305343f1d2b73846ea
akshayxemo <akshaykrdas001@gmail.com>
2023-05-03 00:48:01 +0530

    Portfolio website initiation

---
+++ Task-04-Portfolio-website/assets/js/app.js
@@ -0,0 +1,8 @@
+// let socialLinkHeader = document.getElementById('social-links-header');
+// let socialLinkWidth = socialLinkHeader.clientWidth;
+// let headerWidth = document.getElementById("header").clientWidth;
+// let leftSocial = ((headerWidth-socialLinkWidth)/2);
+// socialLinkHeader.style.left = `${leftSocial}px`;
+// console.log(headerWidth)
+// console.log(socialLinkWidth)
+// console.log(leftSocial)
