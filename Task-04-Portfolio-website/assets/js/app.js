140d48414006aed53c4cfc9957edf8c39afd1302
akshayxemo <akshaykrdas001@gmail.com>
2023-05-04 00:40:16 +0530

    navbar added

--- Task-04-Portfolio-website/assets/js/app.js
+++ Task-04-Portfolio-website/assets/js/app.js
@@ -1,8 +1,35 @@
-// let socialLinkHeader = document.getElementById('social-links-header');
-// let socialLinkWidth = socialLinkHeader.clientWidth;
-// let headerWidth = document.getElementById("header").clientWidth;
-// let leftSocial = ((headerWidth-socialLinkWidth)/2);
-// socialLinkHeader.style.left = `${leftSocial}px`;
-// console.log(headerWidth)
-// console.log(socialLinkWidth)
-// console.log(leftSocial)
+window.onscroll = ()=>{
+    scrollNavFix();
+}
+const nav = document.getElementById("NAV");
+function scrollNavFix(){
+    if(document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        nav.classList.remove("fade-in");
+        nav.classList.add("tilt-in-top-1");
+        nav.classList.add("fixed");
+    }
+    else{
+        nav.classList.remove("tilt-in-top-1")
+        nav.classList.remove("fixed");
+    }
+}
+
+        const list = document.getElementById("collapsed");
+        const menu = document.getElementsByClassName("nav");
+        let count = 0;
+        function ShowHideMenu(){
+            count++;
+            if(count == 1){
+                menu[0].style.top = "0px";
+                menu[0].style.transition = "1s ease"
+                list.classList.remove("fa-bars-staggered");
+                list.classList.add("fa-xmark");
+            }
+            else {
+                menu[0].style.top = "-300px";
+                menu[0].style.transition = "1s ease"
+                list.classList.remove("fa-xmark");
+                list.classList.add("fa-bars-staggered");
+                count = 0;
+            }
+        }
